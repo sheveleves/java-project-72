2e4c12353778ee654450f33cd0354f99efc86365
sheveleves <shevelve@gmail.com>
2023-08-14 17:50:12 +0500

    Step 7.4 add post checkUrl handler

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,9 +1,12 @@
 package hexlet.code.controllers;
 
 import hexlet.code.domain.Url;
+import hexlet.code.domain.UrlCheck;
 import hexlet.code.domain.query.QUrl;
 import io.ebean.PagedList;
 import io.javalin.http.Handler;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 
 import java.net.URL;
 import java.util.List;
@@ -62,7 +65,26 @@ public class UrlController {
                 .id.equalTo(id)
                 .findOne();
         ctx.attribute("url", url);
+        List<UrlCheck> urlChecks = url.getUrlChecks();
+        ctx.attribute("urlChecks", urlChecks);
+
         ctx.render("urls/show.html");
+    };
+
+    public static Handler checkUrl = ctx -> {
+        Long id = ctx.pathParamAsClass("id", long.class).getOrDefault(null);
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+        HttpResponse<String> urlResponse = Unirest.get(url.getName()).asString();
+        int statusCode = urlResponse.getStatus();
+        UrlCheck urlCheck = new UrlCheck(statusCode, "title" + url.getName(), "h1" + url.getName(), "description"
+                + url.getName(), url);
+        List<UrlCheck> urlChecks = url.getUrlChecks();
+        urlChecks.add(urlCheck);
+        urlCheck.save();
+        ctx.redirect("/urls/" + id);
+
     };
 }
 
