0c453237c04a3a41c531f5fb41097fdd53e3a12c
sheveleves <shevelve@gmail.com>
2024-02-13 09:45:36 +0600

    refactoring 3. changing app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,47 +1,81 @@
 package hexlet.code;
 
-
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariConfig;
 import hexlet.code.controllers.RootController;
 import hexlet.code.controllers.UrlController;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import io.javalin.rendering.template.JavalinThymeleaf;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 import static io.javalin.apibuilder.ApiBuilder.path;
 import static io.javalin.apibuilder.ApiBuilder.post;
 import static io.javalin.apibuilder.ApiBuilder.get;
 
+@Slf4j
 public class App {
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "8000");
         return Integer.valueOf(port);
     }
 
+    private static String getDatabaseUrl() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:testdb");
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
     private static void addRoutes(Javalin app) {
-        app.get("/", RootController.welcome);
+        app.get("/", RootController::welcome);
         app.routes(() -> {
             path("urls", () -> {
                 path("{id}", () -> {
-                    get(UrlController.showUrl);
-                    post("checks", UrlController.checkUrl);
+                    get(UrlController::showUrl);
+                    post("checks", UrlController::checkUrl);
                 });
-                get(UrlController.listUrls);
-                post(UrlController.addUrl);
+                get(UrlController::listUrls);
+                post(UrlController::addUrl);
             });
         });
 
     }
 
-    public static Javalin getApp() {
+    public static Javalin getApp() throws IOException, SQLException, ClassNotFoundException {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDatabaseUrl());
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        String sql = readResourceFile("schema.sql");
+        log.info(sql);
+        log.info(hikariConfig.getJdbcUrl());
+
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+
         Javalin app = Javalin.create(config -> {
-            if (!isProduction()) {
-                config.plugins.enableDevLogging();
-            }
-            JavalinThymeleaf.init(getTemplateEngine()); });
+            config.plugins.enableDevLogging();
+            JavalinThymeleaf.init(getTemplateEngine());
+        });
+
         addRoutes(app);
 
         app.before(ctx -> {
@@ -50,7 +84,8 @@ public class App {
         return app;
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         Javalin app = getApp();
         app.start(getPort());
     }
@@ -64,13 +99,5 @@ public class App {
         templateResolver.setCharacterEncoding("UTF-8");
         templateEngine.addTemplateResolver(templateResolver);
         return templateEngine;
-    }
-
-    public static String getMode() {
-        return System.getenv().getOrDefault("APP_ENV", "development");
-    }
-
-    private static boolean isProduction() {
-        return getMode().equals("production");
     }
 }
